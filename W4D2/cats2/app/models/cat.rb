8367f1e1e8efa9701614cde613ff5b9bbb68aad5
= <=>
2017-08-22 14:08:51 -0700

    New rails project with postgres

--- W4D2/cats2/app/models/cat.rb
+++ W4D2/cats2/app/models/cat.rb
@@ -2,11 +2,10 @@ class Cat < ApplicationRecord
   validates :birth_date, :color, :name, :sex, :description, presence: true
   validates :color, inclusion: { in: %w(orange calico black grey tiger white),
     message: "%{value} is not a valid color" }
-  # validates :sex, inclusion: {in: %w(M F), message: "%{value} must be M or F"}
+  validates :sex, inclusion: {in: %w(M F), message: "%{value} must be M or F"}
 
 
   def age
     (Date.today - birth_date) / 365
   end
-
 end
--- W4D2/cats2/db/migrate/20170822210217_create_cats.rb
+++ W4D2/cats2/db/migrate/20170822210217_create_cats.rb
@@ -4,11 +4,9 @@ class CreateCats < ActiveRecord::Migration[5.1]
       t.date :birth_date, null: false
       t.string :color, null: false
       t.string :name, null: false
-      t.string :sex, null: false
+      t.string :sex, null: false, limit: 1
       t.text :description, null: false
-
       t.timestamps
     end
-
   end
 end
