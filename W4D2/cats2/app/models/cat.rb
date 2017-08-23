05c62c9de1abe5f823af89155bf29e147daf5104
drstonebraker <drew.stonebraker@gmail.com>
2017-08-22 17:04:45 -0700

    overlapping dates

--- W4D2/cats2/app/models/cat.rb
+++ W4D2/cats2/app/models/cat.rb
@@ -4,6 +4,12 @@ class Cat < ApplicationRecord
     message: "%{value} is not a valid color" }
   validates :sex, inclusion: {in: %w(M F), message: "%{value} must be M or F"}
 
+  has_many :rental_requests,
+    primary_key: :id,
+    foreign_key: :cat_id,
+    class_name: :CatRentalRequest,
+    dependent: :destroy
+
 
   def age
     (Date.today - birth_date) / 365
