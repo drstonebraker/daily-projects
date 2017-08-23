285aa28e485847b92525209fa841ee183f7d9712
= <=>
2017-08-22 18:20:58 -0700

    Finish CatRentalRequest#approve! and #deny!

--- W4D2/cats2/app/models/cat_rental_request.rb
+++ W4D2/cats2/app/models/cat_rental_request.rb
@@ -9,16 +9,38 @@ class CatRentalRequest < ApplicationRecord
     foreign_key: :cat_id,
     class_name: :Cat
 
-  def overlapping_approved_requests
+  def overlapping_requests
     CatRentalRequest.
       where(<<-SQL, cat_id, start_date, end_date, start_date, end_date)
-        cat_rental_requests.status = 'APPROVED' AND
         cat_rental_requests.cat_id = ? AND
         (cat_rental_requests.start_date
         BETWEEN ? AND ?
         OR cat_rental_requests.end_date
         BETWEEN ? AND ?)
       SQL
+  end
+
+  def overlapping_pending_requests
+    overlapping_requests.
+      where(status: "PENDING")
+  end
+
+  def overlapping_approved_requests
+    overlapping_requests.
+      where(status: "APPROVED")
+  end
+
+  def approve!
+    ActiveRecord::Base.transaction do
+      overlapping_pending_requests.each do |request|
+        request.deny!
+      end
+      self.update!(status: 'APPROVED')
+    end
+  end
+
+  def deny!
+    self.update!(status: 'DENIED')
   end
 
   private
--- W4D2/cats2/db/seeds.rb
+++ W4D2/cats2/db/seeds.rb
@@ -8,25 +8,24 @@
 
 ActiveRecord::Base.transaction do
 
-  # 20.times do
-  #   Cat.create({
-  #     name: Faker::Name.name,
-  #     description: Faker::Twitter.status,
-  #     color: %w(orange calico black grey tiger white).sample,
-  #     sex: %w(M F).sample,
-  #     birth_date: "#{(2010..2016).to_a.sample}/0#{(1..9).to_a.sample}/20"
-  #     })
-  #
-  # end
-
   20.times do
+    Cat.create({
+      name: Faker::Name.name,
+      description: Faker::Twitter.status,
+      color: %w(orange calico black grey tiger white).sample,
+      sex: %w(M F).sample,
+      birth_date: "#{(2010..2016).to_a.sample}/0#{(1..9).to_a.sample}/20"
+      })
+
+  end
+
+  60.times do
     start_date = Faker::Date.between(500.days.ago, Date.today)
     end_date = Faker::Date.between(start_date, 500.days.from_now)
     CatRentalRequest.create({
-      cat_id: (1..5).to_a.sample,
+      cat_id: (1..20).to_a.sample,
       start_date: start_date,
-      end_date: end_date,
-      status: "APPROVED"
+      end_date: end_date
       })
   end
 
