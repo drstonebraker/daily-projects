3e473beffc8d527cf0368703d512a34906b7b726
drstonebraker <drew.stonebraker@gmail.com>
2017-08-25 17:21:21 -0700

    rollback premature migrations

--- w4d5/music_app/db/schema.rb
+++ w4d5/music_app/db/schema.rb
@@ -10,28 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825215834) do
+ActiveRecord::Schema.define(version: 20170825215809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "albums", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "yr"
+    t.integer "band_id", null: false
+    t.boolean "is_live", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["band_id"], name: "index_albums_on_band_id"
   end
 
   create_table "bands", force: :cascade do |t|
     t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "notes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tracks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
