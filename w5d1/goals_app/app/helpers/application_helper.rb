fb400f6c02a198c7e0f929a60bcc34d8de13c0db
drstonebraker <drew.stonebraker@gmail.com>
2017-08-28 13:22:51 -0700

    Write helpers and error partial

--- w5d1/goals_app/app/helpers/application_helper.rb
+++ w5d1/goals_app/app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def auth_token
+    html = <<-HTML
+      <input type="hidden" name="authorization_token" value="<%= form_authorization_token %>">
+    HTML
+    html.html_safe
+  end
+
+  def patch_override
+    html = <<-HTML
+      <input type="hidden" name="_method" value="PATCH">
+    HTML
+    html.html_safe
+  end
 end
