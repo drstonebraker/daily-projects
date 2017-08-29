2427e335aed6e88571e0d641da6dec9a490af0d6
drstonebraker <drew.stonebraker@gmail.com>
2017-08-29 13:24:10 -0700

    Run create subs migration

--- w5d2/reddit_clone/app/models/sub.rb
+++ w5d2/reddit_clone/app/models/sub.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: subs
+#
+#  id           :integer          not null, primary key
+#  title        :string           not null
+#  description  :text
+#  moderator_id :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Sub < ApplicationRecord
 end
