a6c9c98c52579f32d83e89f3ee9496b7c653e86e
drstonebraker <drew.stonebraker@gmail.com>
2017-08-29 11:32:14 -0700

    Write user and session controller methods

--- w5d2/reddit_clone/app/controllers/users_controller.rb
+++ w5d2/reddit_clone/app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      redirect_to root_url
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
 
   def edit
@@ -15,8 +24,15 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
