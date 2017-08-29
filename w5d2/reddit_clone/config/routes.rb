a2c855e3458d575f79ee97cb405695d6173332e7
drstonebraker <drew.stonebraker@gmail.com>
2017-08-29 14:45:55 -0700

    Test subs features

--- w5d2/reddit_clone/config/routes.rb
+++ w5d2/reddit_clone/config/routes.rb
@@ -1,24 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'subs/new'
-
-  get 'subs/create'
-
-  get 'subs/edit'
-
-  get 'subs/update'
-
-  get 'subs/index'
-
-  get 'subs/show'
-
-  get 'subs/destroy'
-
   resource :session, only: %i(new create destroy)
 
   resources :users
+  resources :subs
 
-  root to: 'users#index'
+  root to: 'subs#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
