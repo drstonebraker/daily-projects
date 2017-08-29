0c91bdf0055b18ee1f562bb8740305dcbb281097
drstonebraker <drew.stonebraker@gmail.com>
2017-08-29 13:39:32 -0700

    Write subs controller methods

--- w5d2/reddit_clone/app/controllers/subs_controller.rb
+++ w5d2/reddit_clone/app/controllers/subs_controller.rb
@@ -1,10 +1,22 @@
 class SubsController < ApplicationController
+  before_action :require_user!, only: %i(new create edit update destroy)
+  before_action :require_ownership!, only: %i(edit update destroy)
+
+  def index
+    @subs = Sub.all
+  end
+
+  def show
+    @sub = Sub.find(params[:id])
+  end
+
   def new
     @sub = flash[:sub] || Sub.new
   end
 
   def create
     @sub = Sub.new(sub_params)
+    @sub.moderator_id = current_user.id
     if @sub.save
       redirect_to sub_url(@sub)
     else
@@ -15,18 +27,28 @@ class SubsController < ApplicationController
   end
 
   def edit
+    @sub = flash[:sub] || Sub.new
   end
 
   def update
-  end
-
-  def index
-  end
-
-  def show
+    @sub = Sub.new(sub_params)
+    if @sub.save
+      redirect_to sub_url(@sub)
+    else
+      flash[:errors] = @sub.errors
+      flash[:sub] = @sub
+      redirect_to new_sub_url
+    end
   end
 
   def destroy
+    sub_reddit = Sub.find(params[:id])
+    if sub_reddit.destroy
+      redirect_to root_url
+    else
+      flash[:errors] = sub_reddit.errors.full_messages
+      redirect_to = sub_url(params[:id])
+    end
   end
 
   private
