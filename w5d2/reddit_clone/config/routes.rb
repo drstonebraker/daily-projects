1ae05b0d8756a84fa00e73cc9af0b94698d81fd4
drstonebraker <drew.stonebraker@gmail.com>
2017-08-29 13:19:53 -0700

    Generate Sub model and controller

--- w5d2/reddit_clone/config/routes.rb
+++ w5d2/reddit_clone/config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'subs/new'
+
+  get 'subs/create'
+
+  get 'subs/edit'
+
+  get 'subs/update'
+
+  get 'subs/index'
+
+  get 'subs/show'
+
+  get 'subs/destroy'
+
   resource :session, only: %i(new create destroy)
 
   resources :users
