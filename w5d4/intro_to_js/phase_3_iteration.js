6f32e8c76171d18e72d6aa6d91e5edfdeb836b38
drstonebraker <drew.stonebraker@gmail.com>
2017-08-31 14:18:57 -0700

    Finish phase 3

---
+++ w5d4/intro_to_js/phase_3_iteration.js
@@ -0,0 +1,39 @@
+Array.prototype.bubbleSort = function bubbleSort() {
+  let swapped = true;
+
+  while (swapped) {
+    swapped = false;
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (this[i] > this[j]) {
+          let high = this[i];
+          let low = this[j];
+          this[i] = low;
+          this[j] = high;
+          swapped = true;
+        }
+      }
+    }
+
+  }
+  return this;
+};
+
+String.prototype.substrings = function substrings() {
+  const subs = [];
+
+  for (let i = 0; i < this.length; i++) {
+    for (let j = i + 1; j <= this.length; j++) {
+      subs.push(this.slice(i, j));
+    }
+  }
+  return subs;
+};
+
+
+
+
+
+
+// 'cat' => ['c', 'ca', 'cat', 'a', 'at', 't']
