3df80692f142c59d1326816d0f94bd9fbd2937ae
drstonebraker <drew.stonebraker@gmail.com>
2017-09-12 15:13:37 -0700

    Add Thunk Middleware

--- w7d1/todo_list_redux/frontend/store/store.js
+++ w7d1/todo_list_redux/frontend/store/store.js
@@ -1,7 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
+import thunkMiddleWare from '../middleware/thunk';
 
-const configureStore = () => createStore(rootReducer);
+
+const configureStore = (initialState = {}) => createStore(rootReducer,initialState,applyMiddleware(thunkMiddleWare));
 
 export default configureStore;
 
