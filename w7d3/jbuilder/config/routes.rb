85144852b13d1a288b4eac396f08262949d1edef
drstonebraker <drew.stonebraker@gmail.com>
2017-09-13 13:22:47 -0700

    Write gifts show and index views

--- w7d3/jbuilder/config/routes.rb
+++ w7d3/jbuilder/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       resources :gifts, only:[:index]
     end
 
-    resources :gifts, only:[:show]
+    resources :gifts, only:[ :show]
 
     resources :parties, only:[:show, :index]
   end
