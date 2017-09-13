279eb5632049fa3289b332918b5ca951d523f88f
drstonebraker <drew.stonebraker@gmail.com>
2017-09-13 16:48:27 -0700

    create store

--- w7d3/pokedex/frontend/pokedex.jsx
+++ w7d3/pokedex/frontend/pokedex.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
 import {receiveAllPokemon} from './actions/pokemon_actions';
 import {fetchAllPokemon} from './util/api_util';
 
 window.receiveAllPokemon = receiveAllPokemon;
 window.fetchAllPokemon = fetchAllPokemon;
 
+
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
