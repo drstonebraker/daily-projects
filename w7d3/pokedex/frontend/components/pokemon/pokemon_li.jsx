a7d862e0c8794b4981aa34623eea7013b816e616
drstonebraker <drew.stonebraker@gmail.com>
2017-09-14 13:28:56 -0700

    Connect HashRouter

--- w7d3/pokedex/frontend/components/pokemon/pokemon_li.jsx
+++ w7d3/pokedex/frontend/components/pokemon/pokemon_li.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const PokemonLi = ({pokemon}) => (
+const PokemonLi = ({ pokemon }) => (
   <li>
     {pokemon.name}
     <img src={pokemon.image_url} className="small"></img>
